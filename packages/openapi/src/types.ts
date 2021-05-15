a9fd3719a410c02bcb3b68ca5f3818fa9e951dec
Colin Morelli <colin.morelli@gmail.com>
2021-05-15 08:01:43 -0700

    Moving type files from nestjs to openapi

--- packages/openapi/src/types.ts
+++ packages/openapi/src/types.ts
@@ -1,4 +1,4 @@
-import { Operation } from '@salus-js/openapi'
+import { Operation } from './operation'
 
 export type OperationResult<T extends Operation> = T extends Operation<any, any, any, infer TResult>
   ? TResult
