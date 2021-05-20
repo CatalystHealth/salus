bd24373da66c98dcea1bb9af09b1ca69eefc407a
Colin Morelli <colin.morelli@gmail.com>
2021-05-20 15:39:19 -0400

    Fixing tests

--- packages/codec/src/types/literal.spec.ts
+++ packages/codec/src/types/literal.spec.ts
@@ -4,12 +4,24 @@ import { LiteralCodec } from './literal'
 
 describe('Literal Codec', () => {
   const codec = new LiteralCodec('hello')
+  const numberCodec = new LiteralCodec(123)
+  const booleanCodec = new LiteralCodec(true)
 
   executeDecodeTests([
-    decodeSuccessExpectation('parse exactly', codec, 'hello', 'hello'),
+    decodeSuccessExpectation('parse string exactly', codec, 'hello', 'hello'),
+    decodeSuccessExpectation('parse number exactly', numberCodec, 123, 123),
+    decodeSuccessExpectation('parse boolean exactly', booleanCodec, true, true),
     decodeFailureExpectation('reject undefined', codec, undefined, ['', 'must be exactly "hello"']),
     decodeFailureExpectation('reject numbers', codec, 1, ['', 'must be exactly "hello"']),
     decodeFailureExpectation('reject booleans', codec, true, ['', 'must be exactly "hello"']),
-    decodeFailureExpectation('reject strings', codec, 'test', ['', 'must be exactly "hello"'])
+    decodeFailureExpectation('reject strings', codec, 'test', ['', 'must be exactly "hello"']),
+    decodeFailureExpectation('reject undefined for number', numberCodec, undefined, [
+      '',
+      'must be exactly "123"'
+    ]),
+    decodeFailureExpectation('reject undefined for boolean', booleanCodec, undefined, [
+      '',
+      'must be exactly "true"'
+    ])
   ])
 })
