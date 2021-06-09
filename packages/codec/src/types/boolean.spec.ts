66f2e58485f839bc93e31fcde0b620de1fec779a
Colin Morelli <colin.morelli@gmail.com>
2021-06-08 21:06:39 -0700

    Add lenient boolean

--- packages/codec/src/types/boolean.spec.ts
+++ packages/codec/src/types/boolean.spec.ts
@@ -7,6 +7,18 @@ describe('Boolean Codec', () => {
 
   executeDecodeTests([
     decodeSuccessExpectation('parse simple boolean', codec, true, true),
+    decodeSuccessExpectation('parse lenient string', codec.lenient(), 'true', true),
+    decodeSuccessExpectation('parse lenient string', codec.lenient(), 'false', false),
+    decodeSuccessExpectation('parse lenient string', codec.lenient(), 'TRUE', true),
+    decodeSuccessExpectation('parse lenient string', codec.lenient(), 'FALSE', false),
+    decodeFailureExpectation('not parse lenient string when invalid', codec.lenient(), '1', [
+      '',
+      'must be a boolean'
+    ]),
+    decodeFailureExpectation('not parse lenient string when invalid', codec.lenient(), '0', [
+      '',
+      'must be a boolean'
+    ]),
     decodeFailureExpectation('not parse strings', codec, 'test', ['', 'must be a boolean']),
     decodeFailureExpectation('not parse numbers', codec, 123, ['', 'must be a boolean'])
   ])
