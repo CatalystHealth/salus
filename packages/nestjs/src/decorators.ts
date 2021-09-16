b85e09671f82344a1fbd263cff4962cfdf057fc1
Zia Rahaman <45575337+zia-r@users.noreply.github.com>
2021-09-16 17:14:34 -0400

    Store file on the request body (#12)
    If multer (or some other interceptor/middleware) has put a file onto the request push them onto the request body for salus.
    
    Multer is included by default in nestjs so it makes sense to adopt this pattern for the nestjs package.

--- packages/nestjs/src/decorators.ts
+++ packages/nestjs/src/decorators.ts
@@ -50,7 +50,16 @@ export const Input = createParamDecorator((_data: void, ctx: ExecutionContext) =
     throw new Error('Attempting to use @Input() on an non-operation controller')
   }
 
-  const body = operation.decodeBody(request.body)
+  /*
+   * If multer has put a file at request.file push it onto the request body at * request.body.fieldName.
+   *
+   * Multiple files are not currently supported. Their shape is also irregular
+   * They can be in a singular array or an object keyed by fieldName. In either
+   * scenario multiple files can map to the same fieldName
+   */
+  const fieldNameMappedFiles = request.file ? { [request.file.fieldname]: request.file } : undefined
+
+  const body = operation.decodeBody({ ...request.body, ...fieldNameMappedFiles })
   const params = operation.decodeParams(request.params)
   const query = operation.decodeQuery(request.query)
 
