e6e8d202e77973be65ad67846560f4a71a96c9d4
Andy O'Neill <andy@potatoriot.com>
2022-05-27 16:20:57 -0400

    [LazyCodec] Add optional(), nullable(), document()
    LazyCodec doesn't extend BaseCodec, so it has been missing the common helper
    methods that let us chain .optional() or .document(). There's nothing stopping
    us from defining those helpers on LazyCodec.
    
    - Implement LazyCodec.with()
    - Copy-paste optional(), nullable, and document() from BaseCodec

--- packages/codec/src/types/lazy.ts
+++ packages/codec/src/types/lazy.ts
@@ -2,12 +2,16 @@ import { Codec } from '../codec'
 import { Context } from '../context'
 import { Validation } from '../validation'
 
+import { CodecOptions } from './base'
+import { NullableCodec } from './nullable'
+import { OptionalCodec } from './optional'
+
 export class LazyCodec<A, O> extends Codec<A, O> {
   readonly _tag = 'LazyCodec' as const
 
   private internalCache: Codec<A, O> | null = null
 
-  constructor(public readonly resolver: () => Codec<A, O>) {
+  constructor(public readonly resolver: () => Codec<A, O>, readonly options?: CodecOptions<A>) {
     super()
   }
 
@@ -29,5 +33,26 @@ export class LazyCodec<A, O> extends Codec<A, O> {
 
   public decode(value: unknown, context: Context = Context.create(this)): Validation<A> {
     return this.codec.decode(value, context.replace(this.codec))
+  }
+
+  public optional(): OptionalCodec<A, O> {
+    return new OptionalCodec(this)
+  }
+
+  public nullable(): NullableCodec<A, O> {
+    return new NullableCodec(this)
+  }
+
+  public document(
+    options: Pick<CodecOptions<A>, 'title' | 'description' | 'example' | 'extensions'>
+  ): this {
+    return this.with({
+      ...this.options,
+      ...options
+    }) as this
+  }
+
+  protected with(options: CodecOptions<A>): LazyCodec<A, O> {
+    return new LazyCodec(this.resolver, options)
   }
 }
