12f2e672f74e9e118e1f174480a2c3cd55527618
Andy O'Neill <andy@potatoriot.com>
2022-05-27 16:20:57 -0400

    [LazyCodec] Add optional(), nullable(), document()
    LazyCodec doesn't extend BaseCodec, so it has been missing the common helper
    methods that let us chain .optional() or .document(). There's nothing stopping
    us from defining those helpers on LazyCodec.
    
    - Implement LazyCodec.with()
    - Copy-paste optional(), nullable, and document() from BaseCodec

--- packages/codec/src/types/lazy.spec.ts
+++ packages/codec/src/types/lazy.spec.ts
@@ -13,4 +13,17 @@ describe('Lazy Codec', () => {
       'must be a string'
     ])
   ])
+
+  const optionalCodec = new LazyCodec(() => new StringCodec())
+    .optional()
+    .document({ description: 'Blah blah' })
+
+  executeDecodeTests([
+    decodeSuccessExpectation('parse simple string', optionalCodec, 'one', 'one'),
+    decodeSuccessExpectation('parse undefined', optionalCodec, undefined, undefined),
+    decodeFailureExpectation('reject invalid values for inner codec', optionalCodec, 1, [
+      '',
+      'must be a string'
+    ])
+  ])
 })
