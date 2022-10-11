e6530ac0a89143af641afb28cf7c2fc917d6fbe4
Zia Rahaman <zia@sourcehealth.com>
2022-10-11 11:24:26 -0400

    Fix error messages for min and max

--- packages/codec/src/types/number.spec.ts
+++ packages/codec/src/types/number.spec.ts
@@ -8,6 +8,8 @@ describe('Number Codec', () => {
   executeDecodeTests([
     decodeSuccessExpectation('parse simple number', codec, 1, 1),
     decodeSuccessExpectation('parse simple number with bounds', codec.min(1).max(3), 2, 2),
+    decodeSuccessExpectation('parse simple number with bounds', codec.min(1).max(3), 1, 1),
+    decodeSuccessExpectation('parse simple number with bounds', codec.min(1).max(3), 3, 3),
     decodeSuccessExpectation('parse lenient string', codec.lenient(), '10', 10),
     decodeFailureExpectation('not parse strings', codec, 'test', ['', 'must be a number']),
     decodeFailureExpectation('not parse booleans', codec, false, ['', 'must be a number']),
@@ -15,8 +17,8 @@ describe('Number Codec', () => {
       '',
       'must be a number'
     ]),
-    decodeFailureExpectation('respect min', codec.min(1), 0, ['', 'must be greater than 1']),
-    decodeFailureExpectation('respect max', codec.max(1), 2, ['', 'must be less than 1']),
+    decodeFailureExpectation('respect min', codec.min(1), 0, ['', 'must be at least 1']),
+    decodeFailureExpectation('respect max', codec.max(1), 2, ['', 'must be at most 1']),
     decodeFailureExpectation('respect integer', codec.integer(), 2.5, ['', 'must be an integer']),
     decodeFailureExpectation('respect integer', codec.integer(), 2.5, ['', 'must be an integer'])
   ])
